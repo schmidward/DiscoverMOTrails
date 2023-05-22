2a1828691d9ea6efa00373c88b6c03fd7a2cddf4
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-05-21 19:47:56 -0500

    third commit

--- backend/src/main/java/com/discovermotrails/securitybackend/filter/JWTTokenGeneratorFilter.java
+++ backend/src/main/java/com/discovermotrails/securitybackend/filter/JWTTokenGeneratorFilter.java
@@ -1,6 +1,5 @@
 package com.discovermotrails.securitybackend.filter;
 
-import com.discovermotrails.securitybackend.constants.SecurityConstants;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
--- backend/src/main/java/com/discovermotrails/securitybackend/filter/JWTTokenValidatorFilter.java
+++ backend/src/main/java/com/discovermotrails/securitybackend/filter/JWTTokenValidatorFilter.java
@@ -1,6 +1,5 @@
 package com.discovermotrails.securitybackend.filter;
 
-import com.discovermotrails.securitybackend.constants.SecurityConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
